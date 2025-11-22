5ab4cbd3df7a715a81f4dd5a875de37cc09b8205
Wesley Souza <wesley_luiz2001@hotmail.com>
2025-11-22 15:15:07 +0000

    pqp2

--- src/Album.kt
+++ src/Album.kt
@@ -1,7 +1,7 @@
 class Album(
     val songs: List<Song>
 ) {
-    fun filterByArtist(artist: String): List<Song> {
+    fun filterSongsByArtist(artist: String): List<Song> {
         // filter to keep only songs where song.artist == artist
         return songs.filter { song -> song.artist.contains(artist) }
     }
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
 fun main() {
+    val s1 = Song("Track One", "Artist A", 3.5)
+    val s2 = Song("Track Two", "Artist B", 3.7)
+    val s3 = Song("Track Third", "Artist C", 3.2)
 
+    val album = Album(listOf(s1, s2, s3))
+
+    // call the method
+    val artistASongs = album.filterSongsByArtist("Artist A")
+
+    println("Songs by Artist A:")
+    for (song in artistASongs) {
+        println("_ ${song.title}")
+    }
 }
